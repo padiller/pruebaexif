262eda26a038cdabdcc446b1de3d56153a030096
Luis Padilla <luis.padilla.93@gmail.com>
2017-04-06 01:33:09 -0500

    Regreso

--- app.js
+++ app.js
@@ -2,6 +2,10 @@
 
 //------------------------------------------------------------------------------
 // node.js starter application for Bluemix
+/*eslint-env node*/
+
+//------------------------------------------------------------------------------
+// node.js starter application for Bluemix
 //------------------------------------------------------------------------------
 
 // This application uses express as its web server
@@ -26,41 +30,3 @@ app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });
-
-
-
-
-    document.getElementById("file-input").onchange = function(e) {
-        EXIF.getData(e.target.files[0], function() {
-            var latitude = EXIF.getTag(this, "GPSLatitude"),
-                longitude = EXIF.getTag(this, "GPSLongitude");
-            alert("Latitud: " + (latitude[0] + (latitude[1]/60) + (latitude[2]/3600)) + "\nLongitud: " + (longitude[0] + (longitude[1]/60) + (longitude[2]/3600)));
-        });
-
-
-    }
-
-
-
-  var map, heatmap;
-
-  function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 16,
-      center: {lat: 19.371413, lng: -99.263024},
-      mapTypeId: 'roadmap'
-    });
-
-    heatmap = new google.maps.visualization.HeatmapLayer({
-      data: getPoints(),
-      map: map
-    });
-  }
-
-  function getPoints() {
-    return [
-      {location: new google.maps.LatLng(19.371231, -99.260835), weight: 3}
-    ];
-  }
-
-
